9da517b7faf18751b816e9b139340d6cd0b3d943
Seamus Lowry <spl1owry3@gmail.com>
2021-05-03 10:49:32 -0400

    try flake

--- favorite_tweets/favorite_tweets.py
+++ favorite_tweets/favorite_tweets.py
@@ -3,7 +3,7 @@ approved_list = [54438886]
 
 def favorite_tweets(api):
     all_mentions = api.mentions_timeline(100)
-    needs_favorite = [mention for mention in all_mentions 
-    if mention.user.id in approved_list and not mention.favorited]
+    needs_favorite = [mention for mention in all_mentions
+        if mention.user.id in approved_list and not mention.favorited]
     for mention in needs_favorite:
         print(mention.text)
