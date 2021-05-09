282500c334461b4b92aa42170f9352db78fc353e
Seamus Lowry <spl1owry3@gmail.com>
2021-05-08 21:57:42 -0400

    flake

--- get_id.py
+++ get_id.py
@@ -6,10 +6,12 @@ api = get_api()
 def get_user_handler(handle):
     return api.get_user(handle)
 
+
 def get_user():
     print('Enter a user identifier: ', end='')
     name = input()
     return get_user_handler(name)
+
 
 def get_user_id():
     user = get_user()
