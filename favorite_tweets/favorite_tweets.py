2b3d41257aa773db3295ce41b60099613128e1a2
Seamus Lowry <spl1owry3@gmail.com>
2021-05-08 21:35:32 -0400

    read config file for approved users

--- favorite_tweets/favorite_tweets.py
+++ favorite_tweets/favorite_tweets.py
@@ -1,7 +1,16 @@
-approved_list = [54438886]
+import commentjson
+import os
+
+
+def get_approved_users():
+    f = open(f'{os.path.dirname(__file__)}/config.jsonc')
+    return commentjson.load(f)
+
 
 
 def favorite_tweets(api):
+    approved_list = get_approved_users()
+
     all_mentions = api.mentions_timeline(100)
     needs_favorite = [mention for mention in all_mentions
                       if mention.user.id in approved_list
