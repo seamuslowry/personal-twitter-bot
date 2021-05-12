b12f6f9e13d8c33e8b1339aa3925dc2e77db3648
Seamus Lowry <spl1owry3@gmail.com>
2021-05-12 15:24:07 -0400

    try again

--- entrypoint.py
+++ entrypoint.py
@@ -3,12 +3,11 @@ from favorite_tweets.favorite_tweets import favorite_tweets
 
 api = get_api()
 
+print (api)
+
 
 def entry_favorite_tweets():
     favorite_tweets(api)
     return {
         "statusCode": 200
     }
-
-
-entry_favorite_tweets()
